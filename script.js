754d56e54e264c762a88c948fb977bb3345c2776
Asel Alimbaeva <asel_19.86@mail.ru>
2021-09-12 04:52:41 +0300

    added tasks: draft, english, guest-game, func-retun, function

--- script.js
+++ script.js
@@ -151,3 +151,281 @@ alert("Среднее арифметическое двух чисел ," + c);
 
 alert('При конвертации у вас получилось: ' + dollarExch.toFixed(2) + " Доллар");
 
+
+//-----------draft --------------
+
+
+// 1-  принимать на вход числа и вернуть только те числа которые делятся
+// без остатка на 2
+
+
+function honestArray(array) {
+    var honest = [];
+    for (var i = 0; i != array; i += 1) {
+        if (array[i] % 2 == 0) {
+            honest.push(array[i]);
+        } else {
+            continue;
+        }
+    }
+    return honest
+}
+
+var array = [1, 2, 3, 4];
+alert(honestArray(array));
+
+//---------------- english -------
+
+// 2- выучить новые слова, если выучил то переходим на следующий уровень
+
+function askForWords(words) {
+
+    while (words.length != 0) {
+        var i = parseInt(Math.random() * words.length);
+        var userTranslate = prompt("'Как переводится слово: '" + words[i][0]);
+        if (userTranslate == words[i][1]) {
+            alert('Правильно!');
+            words.splice(i, 1);
+        } else {
+            alert('Правильный ответ: ' + words[i][1]);
+        }
+    }
+
+}
+
+var easyWords = [
+    ['food', 'еда'],
+    ['bike', 'велосипед'],
+    ['apple', 'яблоко'],
+    ['do', 'делать'],
+    ['mean', 'иметь в виду']
+];
+
+askForWords(easyWords);
+
+alert('Поздравляю, вы запомнили все слова! Переходим на следующий уровень!');
+
+var hardWord = [
+    ['chainsaw', 'бензопила'],
+    ['squirrel', 'белка'],
+    ['castle', 'замок'],
+    ['homeland', 'родина'],
+    ['sewerage', 'канализайия'],
+    ['bottom', 'дно']
+];
+
+askForWords(hardWord);
+
+
+//---------- func_retun----------
+
+// ишем факториал, ишем максимальное число
+
+
+function getRandomNumber(max) {
+    // var rand = Math.round(Math.random()* max);
+    // return rand;
+    return Math.round(Math.random() * max);
+}
+
+function max(a, b) {
+    if (a >= b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+function factorial(n) {
+    var result = 1;
+    for (var i = 1; i <= n; i++) {
+        result = result * i;
+    }
+    return result;
+}
+
+var f = factorial(5)
+var a = max(34, 68)
+var c = max(a, 98) + 4;
+var n = getRandomNumber(100);
+var m = getRandomNumber(4);
+
+
+//--------- function ----------
+
+
+// ГЕНЕРАЦИЯ МАССИВА
+function array(n) {
+    // 	var array = []                  //  это примет учителя
+    // 	for(var i=1; i < n; i++);
+    // 		array.push(i);
+    // return array;
+
+    var N = []
+    for (var i = 1; (i + 1) <= (n + 1); N.push(i++));
+    return N;
+}
+
+// СУММА МАССИВА
+// function summ(array){              // ПРИМЕР УЧИТЕЛЯ
+// 	var summ = 0;
+// 	for(var i = 0; i < array.length; i++){
+// 		summ += array[i];
+// 	}
+// 	return summ;
+// }
+function summArray(array) {
+    var summ = 0;
+    for (var i = 0; i != 4; i = i + 1) {
+        summ = summ + array.pop();
+
+    }
+    return summ;
+}
+
+// МАКСИМАЛЬНОЕ ЧИСЛО
+function maxArray(array) {
+    // 	var max = array[0];                         // ПРИМЕР УЧИТЕЛЯ
+    // 	for(var i = 1; i < array.length; i++){
+    // 		if(array[i]> max)
+    // 			max = array[i];
+    // 	}
+    // 	return max;
+    // }
+    var max = 0;
+    for (var i = 0; i != 4; i += 1) {
+        if (max < array[i]) {
+            max = array[i];
+        } else {
+            max = max;
+        }
+    }
+    return max;
+}
+
+//МИНИМАЛЬНОЕ ЧИСЛО
+function minArray(array) {
+    // ПРИМЕР АНАЛАГИЧЕН MAX
+    var min = array[0];
+    for (var i = 0; i != 4; i += 1) {
+        if (min > array[i]) {
+            min = array[i];
+        } else {
+            min = min;
+        }
+    }
+    return min;
+
+}
+// ИШЕМ ЧЕТНОЕ ЧИСЛО
+function honestArray(array) {
+    var honest = [];
+    //for(var i =0; i < array.length; i++)  ПРИМЕР УЧИТЕЛЯ
+    for (var i = 0; i != 4; i += 1) {
+        if (array[i] % 2 == 0) {
+            honest.push(array[i]);
+        } else {
+            continue;
+        }
+    }
+    return honest
+}
+
+
+// var array = array(5);
+// alert(array);
+// alert(summ(array));      ПРИМЕТ УЧИТЕЛЯ . И Т.Д
+
+alert(array(6));
+alert(honestArray(array(6)));
+alert(summArray(array(6)));
+alert(maxArray(array(6)));
+alert(minArray(array(6)));
+
+
+//-------------- guesing-game -------------
+
+// 3. Дописать игру “Угадай число” (по желанию)
+// Сделать игру с двумя игроками и бесконечным числом попыток. 
+// Сделайте возможность выйти из игры по желанию одного из игроков.
+
+var answer = parseInt(Math.random() * 100);
+var playrNumber = 1;
+
+while (true) {
+    var userAnswer = prompt('Угадайте число от 0 до 100.\n Ходит игрок ' + playrNumber + '.\n Для выхода нажмите q');
+
+    if (userAnswer == 'q')
+        break;
+
+    userAnswer = parseInt(userAnswer);
+
+    if (userAnswer == answer) {
+        alert('Поздравляю, победил игрок ' + playrNumber);
+        break;
+    } else if (userAnswer > answer) {
+        alert('Вы ввели слишком большое число!');
+    } else if (userAnswer < answer) {
+        alert('Вы ввели слишком малеькое число!');
+    }
+    if (playrNumber == 1)
+        playrNumber = 2;
+    else
+        playrNumber = 1;
+}
+
+alert('Правильный ответ: ' + answer);
+
+
+//-- вариант второй ---
+
+var answer = parseInt(Math.random() * 100);
+
+while (true) {
+    var userAnswer = prompt('Угадайте число от 0 до 100.  Для выхода нажмите q');
+
+    if (userAnswer == 'q')
+        break;
+
+    userAnswer = parseInt(userAnswer);
+
+    if (userAnswer == answer) {
+        alert('Поздравляю, вы угадали!');
+        break;
+    } else if (userAnswer > answer) {
+        alert('Вы ввели слишком большое число!');
+    } else if (userAnswer < answer) {
+        alert('Вы ввели слишком малеькое число!');
+    }
+}
+
+alert('Правильный ответ: ' + answer);
+
+
+//--- вариант третий -------
+
+var answer = parseInt(Math.random() * 100);
+var userAnswer = +prompt('Угадайте число от  0 до 100');
+var maxTryCount = 2;
+
+for (var tryCount = 1; tryCount < maxTryCount; tryCount++) {
+
+    if (userAnswer == answer) {
+        alert('Поздравляю вы угадали!');
+        break;
+        //continue; сразу переводим к слеуюшкй итерации
+    } else if (userAnswer > answer) {
+        alert('Вы ввели слишком большое число, ');
+    } else if (userAnswer < answer) {
+        alert('Вы ввели слишком маленькое числою, ');
+    }
+
+
+    userAnswer = +prompt('Попробуйте еще раз , введите число от  0 до 100\n У вас осталось ' + (maxTryCount - tryCount) + ' попыток');
+}
+if (tryCount == maxTryCount) {
+    alert('Вы проиграли!');
+    break
+}
+
+document.write('Правильный овет: ' + answer);
